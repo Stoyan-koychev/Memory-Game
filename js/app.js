79a783ea38cfbc569c994b3ce90c80f0ad3161e7
Stoyan Koychev <stoqnkoi4ev@abv.bg>
2018-03-08 22:44:26 +0200

    feat: Adding shuffling function

--- js/app.js
+++ js/app.js
@@ -31,3 +31,21 @@ let cardClicks = 0; // -> Every 2 clicked cards will be equal to one move
 let moveCounter = 0; // -> Move cointer will be shown in the .score-panel
 let savedCards = []; // -> Will hold the clicked cards
 let starsCounter = 0; // -> Will count the shown stars
+let shuffledIconsClasses = []; // -> The shuffled cards will be stored here
+
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+shuffledIconsClasses = shuffle(ICONS_CLASS);
