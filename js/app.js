4a68d3bed545af8a1a1c131aab4c32a4bc19e3f3
Stoyan Koychev <stoqnkoi4ev@abv.bg>
2018-03-08 21:27:34 +0200

    feat: Adding all elements from the DOM to variables

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const CARDS = Array.prototype.slice.call(document.getElementsByClassName('card'));
+const ICON_HANDLERS = document.getElementsByClassName('icon-handler');
+const MOVES = document.getElementsByTagName('span');
+const RESTART_BTN = document.querySelector('button');
+const PLAY_AGAIN_BTN = document.querySelector('.congrats').querySelector('button');
+const STARS_PANEL = document.querySelector('.score').getElementsByTagName('li');
+const STARS = [
+  'far fa-star',
+  'fas fa-star'
+];
+const ICONS_CLASS = [
+  'fab fa-apple',
+  'fab fa-github',
+  'fab fa-behance',
+  'fab fa-html5',
+  'fab fa-angular',
+  'fab fa-js',
+  'fab fa-slack',
+  'fab fa-dribbble',
+  'fab fa-apple',
+  'fab fa-github',
+  'fab fa-behance',
+  'fab fa-html5',
+  'fab fa-angular',
+  'fab fa-js',
+  'fab fa-slack',
+  'fab fa-dribbble'
+];
